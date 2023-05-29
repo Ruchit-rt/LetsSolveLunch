9323e031fb4fb382dc5576f6f092a2345e01de25
Dhruv Jimulia <dj321@ic.ac.uk>
2023-05-29 13:51:29 +0100

    Added app skel to project, renamed project

--- drp/asgi.py
+++ drp/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for appVer1 project.
+ASGI config for drp project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'appVer1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drp.settings')
 
 application = get_asgi_application()
