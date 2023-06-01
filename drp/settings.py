e96412af11fb5ce4a2f38f029283327cb195889b
Manav Jain <manav.jain21@imperial.ac.uk>
2023-06-01 23:33:31 +0100

    Fixed bug with STATIC_ROOT

--- drp/settings.py
+++ drp/settings.py
@@ -4,7 +4,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "assets/")
 STATIC_URL = os.path.join(BASE_DIR, "static/")
 
 # Quick-start development settings - unsuitable for production
