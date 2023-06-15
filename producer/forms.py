ebd9e2804bacad83e82323a3d0f50af34e12cf65
Dhruv Jimulia <dj321@ic.ac.uk>
2023-06-15 21:59:48 +0100

    Added some styling to add menu form

--- producer/forms.py
+++ producer/forms.py
@@ -9,3 +9,14 @@ class MealForm(ModelForm):
         fields = ("meal_id","name","description","picture","number_of_reservations","price_staff","price_student", "restaurant", "tags")
         exclude = ("restaurant",)
         m_tags = TagField()
+
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'text-input'}),
+            'description': forms.TextInput(attrs={'class': 'text-input'}),
+            'picture': forms.FileInput(attrs={'class': 'text-input'}),
+            'number_of_reservations': forms.NumberInput(attrs={'class': 'text-input'}),
+            'price_staff': forms.NumberInput(attrs={'class': 'text-input'}),
+            'price_student': forms.NumberInput(attrs={'class': 'text-input'}),
+            'restaurant': forms.Select(attrs={'class': 'text-input'}),
+            'tags': forms.TextInput(attrs={'class': 'text-input'})
+        }
