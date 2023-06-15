9560fbcd1f80d801f5726afb091e6e3382f75b81
Ruchit Tandon <ruchit10don@gmail.com>
2023-06-15 14:29:54 +0100

    trying wss

--- consumer/templates/dyanmicReserve.js
+++
@@ -1,9 +0,0 @@
-console.log('dynamic reserve happening')
-
-socket = new WebSocket('ws://' + window.location.host + '/ws/dynamic/')
-console.log(document.getElementById("meal_id").textContent)
-socket.onopen = function(e) {
-    socket.send(JSON.stringify({
-        'message': document.getElementById("meal_id").textContent,
-    }));
-};
